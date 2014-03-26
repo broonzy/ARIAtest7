8927196a18071ff9bf1e5403f0c15259e069a3f6
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-03-26 11:09:43 +0000

    added tower text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window._skel_config = {
-    prefix: 'style',
-        preloadStyleSheets: true,
+    prefix: 'css/style',
+    preloadStyleSheets: true,
     resetCSS: true,
     boxModel: 'border',
     grid: { gutters: 30 },
@@ -9,4 +9,28 @@ window._skel_config = {
         narrow: { range: '481-1199', containers: 960 },
         mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
     }
-}
+};
+
+$(document).ready(function() {
+    $("#convertbutton").click(function() {
+        var metricValue = $("#metricinput").val();
+        var imperialValue = $("#imperialinput").val();
+
+
+        if (((metricValue.length !=0) && (imperialValue.length !=0))
+            || ((metricValue.length == 0) && imperialValue.length == 0)) {
+            alert("Please set one field to convert")
+        }
+        if (metricValue.length != 0) {
+            $("#imperialinput").val(Converter.convertFromPoundsToKIlo(metricValue));
+        }
+        else if (imperialValue.length != 0) {
+            $("#metricinput").val(Converter.convertFromMetricToImperial(imperialValue));
+        }
+    });
+    $("#clearbutton").click(function() {
+        $("#imperialinput").val('');
+        $("#metricinput").val('');
+    });
+
+});
